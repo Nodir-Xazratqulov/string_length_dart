843371cdec9d6872a7fbbf780e2a78594045b5a9
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-07-20 11:32:55 +0500

    str-6

--- bin/string_length06.dart
+++ bin/string_length06.dart
@@ -7,6 +7,17 @@
     Returns:
         shortest string
 */
+
+func(String s1, String s2){
+  int a = s1.length;
+  int b = s2.length;
+  if (a>b) {
+    return s2;
+  } else {
+    return s1;
+  }
+}
 void main() {
   // write your code here
+  print(func('code', 'ab'));
 }
